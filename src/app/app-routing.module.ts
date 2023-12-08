b36b29a93f9e1d9099e6ed35a3fade86f5875095
IMT Choc <emilienadam@yahoo.fr>
2023-12-08 11:03:24 +0100

    FIX: changement routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LeagueAppComponent } from './league-app/league-app.component';
-import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 
 const routes: Routes = [
   { path: '', component: MenuComponent},
-  { path: 'league', component: LeagueAppComponent}
+  { path: 'league', component: LeagueAppComponent},
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
